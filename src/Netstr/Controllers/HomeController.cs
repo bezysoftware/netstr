d5866008c2a78da87377d71361a85daae16f4cde
Tomas Bezouska <bezysoftware@outlook.com>
2024-09-27 10:56:36 +0200

    feat(ui): home ui

---
+++ src/Netstr/Controllers/HomeController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Netstr.RelayInformation;
+using Netstr.ViewModels;
+
+namespace Netstr.Controllers
+{
+    [Route("/")]
+    public class HomeController : Controller
+    {
+        private readonly IRelayInformationService service;
+
+        public HomeController(IRelayInformationService service)
+        {
+            this.service = service;
+        }
+
+        [HttpGet]
+        public IActionResult Index()
+        {
+            if (Request.Headers["Accept"] == "application/nostr+json")
+            {
+                return Ok(this.service.GetDocument());
+            }
+            else
+            {
+                var vm = new HomeViewModel(this.service.GetDocument(), $"wss://{Request.Host}");
+                return View(vm);
+            }
+        }
+    }
+}
---
+++ src/Netstr/Views/Home/Index.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Netstr.Views.Home
+{
+    public class HomeModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
